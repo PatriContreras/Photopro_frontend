1b2cbf252d808864d8806dde9cd2be062c39b15c
PatriContreras <patri.contreras91@gmail.com>
2021-03-07 17:52:21 +0100

    Cambiado formato sass a scss

--- src/app/components/perfil-cliente/perfil-cliente.component.ts
+++ src/app/components/perfil-cliente/perfil-cliente.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-perfil-cliente',
   templateUrl: './perfil-cliente.component.html',
-  styleUrls: ['./perfil-cliente.component.sass']
+  styleUrls: ['./perfil-cliente.component.scss']
 })
 export class PerfilClienteComponent implements OnInit {
 
--- src/app/components/perfil-fotografo/perfil-fotografo.component.ts
+++ src/app/components/perfil-fotografo/perfil-fotografo.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-perfil-fotografo',
   templateUrl: './perfil-fotografo.component.html',
-  styleUrls: ['./perfil-fotografo.component.sass']
+  styleUrls: ['./perfil-fotografo.component.scss']
 })
 export class PerfilFotografoComponent implements OnInit {
 
