a4e61d015e7e783cd2b468f27d2f4c0637892f85
PatriContreras <patri.contreras91@gmail.com>
2021-03-11 19:42:16 +0100

    SideBar terminado. Pendiente funcionalidad de filtros.

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,3 +1,5 @@
+import { NONE_TYPE } from '@angular/compiler';
+import { normalizeGenFileSuffix } from '@angular/compiler/src/aot/util';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,17 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalComponent implements OnInit {
 
-  constructor() { }
+  sideNavStyle: any;
+  mainStyle: any;
+  descubrirTexto: any;
+
+  constructor() {
+    this.sideNavStyle = {
+      width: 0,
+    };
+
+    this.mainStyle = {
+      marginLeft: 0,
+    };
+
+  }
 
   ngOnInit(): void {
   }
 
   openNav() {
     console.log('Hola!');
+    this.sideNavStyle = {
+      width: '250px',
+    };
+
+    this.mainStyle = {
+      marginLeft: '250px',
+    }
 
   }
 
   closeNav() {
+    this.sideNavStyle = {
+      width: '0px',
+    }
+
+    this.mainStyle = {
+      marginLeft: 0,
+    }
+
+    this.descubrirTexto = {
+
+    }
 
   }
 
